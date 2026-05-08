65969f62417b4cb3d4422c54b2a4d6ded4af5ba8
Santiago Kuru <ravasisantiago@gmail.com>
2026-05-08 14:05:48 -0300

    Delete app.jsx

--- app.jsx
+++
@@ -1,7 +0,0 @@
-const App = () => (
-  <main style={{ minHeight: "100vh", background: "var(--cream)" }}>
-    <Evaluation />
-  </main>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
